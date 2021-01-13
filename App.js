2b74d5242b2e0308e06c21994f9f8f74a37fec71
mukuul <mukul654@gmail.com>
2021-01-13 12:45:51 +0530

    Memory leak fixing

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, 
-  StatusBar, ActivityIndicator, TextInput, Button, Image, BackHandler } from 'react-native';
+import {
+  SafeAreaView, View, FlatList, StyleSheet, Text,
+  StatusBar, ActivityIndicator, TextInput, Button, Image, BackHandler
+} from 'react-native';
 import { SvgUri } from 'react-native-svg';
 const { fetchCountries, fetchCountry } = require('./fetchCountryData')
 
 
 
 const App = () => {
-  const [ displayData, setdisplayData ] = useState([{name:""}]);
+  const [displayData, setdisplayData] = useState([{ name: "" }]);
   const [text, setText] = useState('');
-  const [ country, setCountry ] = useState(null)
+  const [country, setCountry] = useState(null)
   const initialCountryState = {
     name: "",
     flagUrl: "",
@@ -19,7 +21,7 @@ const App = () => {
     population: 0,
     timezones: [""],
   }
-  const [ countryData, setCountryData ] = useState(initialCountryState)
+  const [countryData, setCountryData] = useState(initialCountryState)
   const [isLoading, setLoading] = useState(true);
   function handleBackButtonClick() {
     setCountry(null);
@@ -28,8 +30,8 @@ const App = () => {
 
   const Item = ({ name }) => (
     <View style={styles.item}>
-      <Text 
-        onPress= {()=>{setCountry(name)}}
+      <Text
+        onPress={() => { setCountry(name) }}
         style={styles.name}
       >
         {name}
@@ -40,14 +42,15 @@ const App = () => {
   const renderItem = ({ item }) => (
     <Item name={item.name} />
   );
-  
+
   useEffect(() => {
+
     fetchCountries()
-    .then((res=>{
-      setdisplayData(res);
-    }))
-    .catch(console.error)
-    .finally(() => setLoading(false));
+      .then((res => {
+        setdisplayData(res);
+      }))
+      .catch(console.error)
+      .finally(() => setLoading(false));
   }, []);
 
   useEffect(() => {
@@ -59,72 +62,76 @@ const App = () => {
 
   useEffect(
     () => {
+      let isMounted = true;
       const ac = new AbortController();
       fetchCountry(country)
-      .then(data => {
-        if(country) {
-          let countryObj = {
-            name: country,
-            flag: data.flag,
-            capital: data.capital,
-            region: data.region,
-            subregion: data.subregion,
-            population: data.population,
-            timezones: data.timezones.join(', '),
-            languages: data.languages.map(obj=>obj.name).join(', ')
-          };
-          setCountryData(countryObj)
-        }
-      })
-      return () => ac.abort();
+        .then(data => {
+          if (country && isMounted) {
+            let countryObj = {
+              name: country,
+              flag: data.flag,
+              capital: data.capital,
+              region: data.region,
+              subregion: data.subregion,
+              population: data.population,
+              timezones: data.timezones.join(', '),
+              languages: data.languages.map(obj => obj.name).join(', ')
+            };
+            setCountryData(countryObj)
+          }
+        })
+      return () => {
+        isMounted = false;
+      }
+      //return () => ac.abort(); //not working
     }, [country]);
 
   return (
     <SafeAreaView style={styles.container}>
-      {isLoading ? <ActivityIndicator/> : (
-        !country ?  (
+      {isLoading ? <ActivityIndicator /> : (
+        !country ? (
           <View style={{ flex: 1, padding: 24 }}>
             <TextInput
-            style={{height: 40}}
-            placeholder="Search for country..."
-            onChangeText={text => setText(text)}
-            defaultValue={text} />
+              style={{ height: 40 }}
+              placeholder="Search for country..."
+              onChangeText={text => setText(text)}
+              defaultValue={text} />
             <FlatList
-            numColumns= {2}
-            horizontal= {false}
-            data={displayData.filter(arr => arr.name.toLowerCase().search(text.toLowerCase()) === 0)}
-            renderItem={renderItem}
-            keyExtractor={item => item.name}
+              numColumns={2}
+              horizontal={false}
+              data={displayData.filter(arr => arr.name.toLowerCase().search(text.toLowerCase()) === 0)}
+              renderItem={renderItem}
+              keyExtractor={item => item.name}
             />
           </View>
         ) : (
-          <View 
-            style={{
-              flex:1,
-              alignItems: 'center',
-            }}
-          >
-            <View style={{flexDirection:'row',flex:0.5 }}>
-              <Text style={{fontSize:30, fontWeight:"bold"}}>{countryData.name}</Text>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+              }}
+            >
+              <View style={{ flexDirection: 'row', flex: 0.5 }}>
+                <Text style={{ fontSize: 30, fontWeight: "bold" }}>{countryData.name}</Text>
+              </View>
+              <View style={{ flex: 2, justifyContent: 'center', aspectRatio: 1 }}>
+                <SvgUri flex='1' width="100%" height="100%" uri={countryData.flag} viewBox="0 0 400 400">
+                </SvgUri>
+              </View>
+              <View style={styles.countryBox}>
+                <Text>Capital : {countryData.capital}</Text>
+                <Text>Region : {countryData.region}</Text>
+                <Text>Sub Region : {countryData.subregion}</Text>
+                <Text>Population : {countryData.population}</Text>
+                <Text>Languages : {countryData.languages}</Text>
+                <Text>Time Zones : {countryData.timezones}</Text>
+              </View>
+              <Button
+                title="Go Back"
+                onPress={() => { setCountry(null) }}
+              />
             </View>
-            <View style={{ flex: 2, justifyContent:'center', aspectRatio:1 }}>
-              <SvgUri flex='1' width="100%" height="100%" uri={countryData.flag} viewBox="0 0 400 400">
-              </SvgUri>
-            </View>
-            <View style={styles.countryBox}> 
-              <Text>Capital : {countryData.capital}</Text>
-              <Text>Region : {countryData.region}</Text>
-              <Text>Sub Region : {countryData.subregion}</Text>
-              <Text>Population : {countryData.population}</Text>
-              <Text>Languages : {countryData.languages}</Text>
-              <Text>Time Zones : {countryData.timezones}</Text>
-            </View>
-            <Button
-              title="Go Back"
-              onPress={()=>{setCountry(null)}}
-            />
-          </View>
-        )
+          )
       )}
     </SafeAreaView>
   );
@@ -148,7 +155,7 @@ const styles = StyleSheet.create({
   },
   countryBox: {
     flex: 3,
-    backgroundColor:"powderblue",
+    backgroundColor: "powderblue",
     // justifyContent: 'center'
   }
 });
